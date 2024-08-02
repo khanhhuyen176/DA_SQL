8c83f97509051abb84980b3cdaf216d813590cf2
Huyen Nguyen <158005570+khanhhuyen176@users.noreply.github.com>
2024-08-02 13:21:45 +0700

    Update DAY3-_exercise.sql

--- DAY3-_exercise.sql
+++ DAY3-_exercise.sql
@@ -50,4 +50,9 @@ where referee_id != 2 or referee_id is null
 --ex13
 SELECT part, assembly_step FROM parts_assembly
 where finish_date is null
-
+--ex14
+select * from lyft_drivers
+where yearly_salary <= 30000 or yearly_salary >=70000
+--ex15
+select advertising_channel from uber_advertising
+where money_spent > 100000 and year = 2019
