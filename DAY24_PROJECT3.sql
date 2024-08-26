c019486b46fbae87189b03d04de27e75f394a40b
Huyen Nguyen <158005570+khanhhuyen176@users.noreply.github.com>
2024-08-26 22:31:04 +0700

    Update DAY24_PROJECT3.sql

--- DAY24_PROJECT3.sql
+++ DAY24_PROJECT3.sql
@@ -87,7 +87,50 @@ where rank = 1
 
 -- 5) Ai là khách hàng tốt nhất, phân tích dựa vào RFM 
 --(sử dụng lại bảng customer_segment ở buổi học 23)
+with cal_rfm as(
+	select
+		customername, 
+		current_date - max(orderdate) as r_score,
+		count(distinct ordernumber) as f_score,
+		sum(sales) as m_score
+	from public.sales_dataset_rfm_prj_clean
+	group by 1
+), chia_khoang as(
+	select 
+		customername,
+		ntile(5) over(order by r_score desc) as R,
+		ntile(5) over(order by f_score) as F,
+		ntile(5) over(order by m_score) as M
+	from cal_rfm 
+), rfm_final as(
+select 
+    a.customername,
+	b.segment
+from (
+		select customername,
+				R :: varchar || f :: varchar || m :: varchar as rfm
+		from chia_khoang) a
+join public.segment_score b
+	on a.rfm = b.scores
+order by segment
+) select customername, segment
+from rfm_final
+where segment = 'Champions'
 
+/* 13 khách hàng tốt nhất:
+Anna's Decorations, Ltd
+Dragon Souveniers, Ltd.
+Corporate Gift Ideas Co.
+Technics Stores Inc.
+Gift Depot Inc.
+The Sharp Gifts Warehouse
+Handji Gifts& Co
+Tokyo Collectables, Ltd
+La Rochelle Gifts
+Euro Shopping Channel
+Diecast Classics Inc.
+Mini Gifts Distributors Ltd.
+Reims Collectables */
 
 
 
